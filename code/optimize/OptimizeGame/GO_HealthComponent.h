72ddf9c0c824254edbddab2146c90d42be0f3780
Shawn <s.carroll.home@gmail.com>
2016-10-24 01:02:45 -0400

    Component: Adding a basic health component that stores a simple health value

---
+++ code/optimize/OptimizeGame/GO_HealthComponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GO_EntityComponent.h"
+
+namespace GO
+{
+	class HealthComponent : public EntityComponent
+	{
+	public:
+		HealthComponent(Entity& anEntity)
+			: EntityComponent(anEntity)
+			, myHealth(1000.0f)
+		{
+		}
+
+		float myHealth;
+
+	private:
+		virtual void update() override {};
+	};
+}
