5ba7f24a511363540aab549a66612c592e651fb2
Shawn Carroll <s.carroll.home@gmail.com>
2017-05-22 16:25:14 -0400

    Systems: The calculate damage system has more even iteration distrubtion
    Due to the fact that the calculate damage system iterates over two lists, the distribution was unfairly weighted towards the earlier iterations. It now attempts to calculate an even work load for each task based on number of iterations

--- code/optimize/OptimizeGame/main.cpp
+++ code/optimize/OptimizeGame/main.cpp
@@ -1,7 +1,5 @@
 #include "StableHeaders.h"
 
-#include <algorithm>
-
 #include "GO_GameUpdateSystem.h"
 #include "GO_TaskIdentifiers.h"
 #include "GO_MovementCalculationSystem.h"
@@ -269,12 +267,12 @@ void RunParallel(Iterator anIterBegin, Iterator anIterEnd, GO::World &aWorld, GO
 				const size_t startIndex = i * NumJobsPerThread;
 				const size_t endIndex = (i + 1) * NumJobsPerThread;
 
-				std::future<int> result = aThreadPool.Submit([&counter, &aProfiler, startIndex, endIndex, anIterBegin, aThreadTag, aCallback]() -> int
+				std::future<int> result = aThreadPool.Submit([&counter, &aProfiler, startIndex, endIndex, anIterBegin, aThreadTag, aCallback, i, threadCount]() -> int
 				{
 					PROFILER_SCOPED(&aProfiler, "Run Parallel", 0x00A2E8FF);
 					// TODO: The thread tag should be automatically gathered from the same tag as the 'submit' event
 					aProfiler.PushThreadEvent(aThreadTag);
-					aCallback(anIterBegin.operator->(), startIndex, endIndex, aProfiler);
+					aCallback(anIterBegin.operator->(), startIndex, endIndex, i, threadCount, aProfiler);
 
 					const size_t numProcessed = endIndex - startIndex;
 
@@ -290,7 +288,7 @@ void RunParallel(Iterator anIterBegin, Iterator anIterEnd, GO::World &aWorld, GO
 			{
 				const size_t startIndex = (threadCount - 1) * NumJobsPerThread;
 				const size_t numProcessed = numElements - startIndex;
-				aCallback(anIterBegin.operator->(), startIndex, numElements, aProfiler);
+				aCallback(anIterBegin.operator->(), startIndex, numElements, threadCount - 1, threadCount, aProfiler);
 				counter.fetch_add(numProcessed);
 			}
 
@@ -307,7 +305,7 @@ void RunParallel(Iterator anIterBegin, Iterator anIterEnd, GO::World &aWorld, GO
 	else
 	{
 		// Run everything locally since it's a small batch anyways
-		aCallback(anIterBegin.operator->(), 0, numElements, aProfiler);
+		aCallback(anIterBegin.operator->(), 0, numElements, 0, 1, aProfiler);
 	}
 }
 
@@ -362,8 +360,9 @@ namespace GameEvents
 		ourCombatDamageMessages.push_back(msg);
 	}
 
-	void QueueDamage(const std::vector<CombatDamageMessage>& someDamageMessages)
+	void QueueDamage(const std::vector<CombatDamageMessage>& someDamageMessages, GO_APIProfiler& aProfiler)
 	{
+		PROFILER_SCOPED(&aProfiler, "QueueDamage", 0xd986e8ff);
 		std::lock_guard<std::mutex> lock(ourDamageQueueMutex);
 		ourCombatDamageMessages.insert(ourCombatDamageMessages.end(), someDamageMessages.begin(), someDamageMessages.end());
 	}
@@ -403,7 +402,7 @@ namespace GO
 			const size_t entityListSize = entityList.size();
 
 			RunParallel(entityList.begin(), entityList.end(), world, threadPool, profiler, GO_ProfilerTags::THREAD_TAG_CALC_GAME_TASK,
-				[&](GO::Entity** someEntities, const size_t aStartIndex, const size_t anEndIndex, GO_APIProfiler& aProfiler)
+				[&](GO::Entity** someEntities, const size_t aStartIndex, const size_t anEndIndex, const size_t aTaskIndex, const unsigned int aTotalTaskCount, GO_APIProfiler& aProfiler)
 			{
 				PROFILER_SCOPED(&profiler, "CalculateCombatDamageSystem_Threaded", 0x992200ff);
 				const GO::World::EntityList& entityList = world.getEntities();
@@ -411,9 +410,56 @@ namespace GO
 
 				std::vector<CombatDamageMessage> damageMessages;
 
-				for (size_t outerIndex = aStartIndex; outerIndex < anEndIndex; outerIndex++)
+				const size_t numTotalIterations = (entityListSize * (entityListSize - 1)) / 2;
+				const size_t numIterationsPerTask = (numTotalIterations + aTotalTaskCount - 1) / aTotalTaskCount;
+
+				// An over estimated iterations required by this task has now been calculated. The cut off
+				// iteration count will be based on full passes of the inner loop. That is, the inner loop
+				// will never do partial iterations. At most, a task will perform one full inner iteration
+				// more than a perfectly balanced system
+
+				// We need to loop over all the previous task indices and figure out how many tasks they will
+				// be taking. We can then start ours based on this.
+				// TODO(scarroll): This can be solved by subtracting the summation of values before our desired
+				//					n and solving.
+
+				// https://stackoverflow.com/questions/362059/what-is-the-big-o-of-a-nested-loop-where-number-of-iterations-in-the-inner-loop/362092#362092
+				// Calculate n such that summation equals numIterationsPerTask
+				// sum = n(n+1) / 2
+				// sum * 2 = n * (n+1)
+				// sum * 2 = n^2 + n
+				// sum * 2 = n^2 + n + (1/2)^2 - (1/2)^2
+				// sum * 2 = (n + 0.5)^2 - (0.5)^2
+				// sum * 2 + 0.25 = (n + 0.5)^2
+				// sqrt(sum * 2 + 0.25) = n + 0.5
+				// sqrt(sum * 2 + 0.25) - 0.5 = n
+
+				std::vector<int> taskSumCounts(aTotalTaskCount);
+
+				std::vector<std::pair<int, int>> taskIndices(aTotalTaskCount);
+				for(int i = 0; i < aTotalTaskCount; i++)
 				{
-					PROFILER_SCOPED(&profiler, "Testing Each Entity", 0xff5500ff);
+					int sumRequired = (i > 0) ? (taskSumCounts[i - 1] + numIterationsPerTask) : numIterationsPerTask;
+					taskSumCounts[i] = sumRequired;
+
+					std::pair<int, int> indices;
+					indices.first = (i > 0) ? taskIndices[i - 1].second : 0;
+					indices.second = std::round(sqrt(sumRequired * 2 + 0.25) - 0.5);
+
+					taskIndices[i] = indices;
+				}
+
+				for(int i = 0; i < aTotalTaskCount; i++)
+				{
+					std::pair<int, int>& indices = taskIndices[i];
+
+					std::swap(indices.first, indices.second);
+					indices.first = entityListSize - indices.first - 1;
+					indices.second = entityListSize - indices.second - 1;
+				}
+
+				for(size_t outerIndex = taskIndices[aTaskIndex].first; outerIndex < taskIndices[aTaskIndex].second; outerIndex++)
+				{
 					GO::Entity* outerEntity = someEntities[outerIndex];
 					GO_ASSERT(outerEntity, "Entity list contains a nullptr");
 
@@ -445,7 +491,7 @@ namespace GO
 				}
 
 				// TODO(scarroll): Perform the gather in parallel with other tasks
-				GameEvents::QueueDamage(damageMessages);
+				GameEvents::QueueDamage(damageMessages, profiler);
 			});
 		}
 	};
@@ -466,7 +512,7 @@ namespace GO
 }
 
 
-void ApplyCombatDamageInternal(const CombatDamageMessage* someCombatMessages, const size_t aStartIndex, const size_t anEndIndex, GO_APIProfiler& aProfiler)
+void ApplyCombatDamageInternal(const CombatDamageMessage* someCombatMessages, const size_t aStartIndex, const size_t anEndIndex, const size_t aTaskIndex, const unsigned int aTotalTaskCount, GO_APIProfiler& aProfiler)
 {
 	PROFILER_SCOPED(&aProfiler, "ApplyCombatDamageInternal", 0x6B0BBFFF);
 	for (size_t currentIndex = aStartIndex; currentIndex < anEndIndex; currentIndex++)
